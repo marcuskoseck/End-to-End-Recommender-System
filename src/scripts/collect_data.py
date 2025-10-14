f1c29fb75e13567d333ab3def985c7ee2d398bb4
marcus.koseck <marcuskoseck@gmail.com>
2025-10-13 19:19:30 -0500

    Made small updates to parse data. Need to make changes so the data being parsed is specific to the data type.

--- src/scripts/collect_data.py
+++ src/scripts/collect_data.py
@@ -3,16 +3,16 @@ import json
 import numpy as np
 import pandas as pd
 
-def obtain_raw_data():
+def obtain_raw_data(pageNum):
     base_url = "https://api.nal.usda.gov/fdc/v1/foods/list"
     api_key = "NhfPgJChrCiU2JaNEcZRTNkdkMw6RDKLhBsRarou"
     #query = "tomato"
     params = {
         "api_key": api_key,
         #"query": query,
-        "dataType": "Foundation",
+        "dataType": "Branded",
         "pageSize": 200,
-        "pageNumber": 1
+        "pageNumber": pageNum
     }
     url = base_url + "?" + urllib.parse.urlencode(params)
     req = urllib.request.Request(url, headers={"Accept": "application/json"})
@@ -26,10 +26,11 @@ def obtain_raw_data():
         #data.get("foods", [])  # List of food objects
     return data
 
-def save_raw_json(json_data):
+def save_raw_json(page_num,json_data):
     df = pd.json_normalize(json_data)
-    df.to_parquet(r'C:\marcus_projects\End-to-End-Recommender-System\data\raw_data\dataframe.parquet', compression='brotli')
+    df.to_parquet(rf'C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data\dataframe_{page_num}.parquet', compression='brotli')
 
-save_raw_json(obtain_raw_data())
+for pageNum in range(1,4):
+    save_raw_json(pageNum,obtain_raw_data(pageNum))
 
 
--- src/scripts/store_data.py
+++ src/scripts/store_data.py
@@ -20,17 +20,35 @@ class Data_organizer:
     def open_database(self):
         return sqlite3.connect("USDA_foods.db")
 
-    def make_tables(self):
-        
-        cur = self.con.cursor()
-        cur.execute("CREATE TABLE foods(fdcId, Description, datatype, publicationDate, ndbNumber")
-
     def fill_tables(self):
         
-        raw_data_dir = r"C:\marcus_projects\End-to-End-Recommender-System\data\raw_data"
+        raw_data_dir = r"C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data"
         data_files = os.listdir(raw_data_dir)
+        all_foods_dfs = []
+        all_nut_dfs = []
+        all_food_nut_dfs = []
         for data_path in data_files:
             df = self.open_parquet(os.path.join(raw_data_dir,data_path))
-            breakpoint()
+            place_holder = df.explode("foodNutrients",ignore_index=True)
+            df = pd.concat(
+                [
+                place_holder.drop(columns=["foodNutrients"]),
+                 place_holder["foodNutrients"].apply(pd.Series)
+                ],
+                axis=1
+            )
+            try:
+                foods_df = df[["fdcId","description","dataType","publicationDate","ndbNumber"]]
+                nutrients_df =  df[["number","name","unitName"]]
+                food_nut_df = df[["fdcId","number","amount","derivationCode","derivationDescription"]]
+            except:
+                breakpoint()
+            # all_foods_dfs.append(foods_df)
+            # all_nut_dfs.append(nutrients_df)
+            # all_food_nut_dfs.append(food_nut_df)
+            # breakpoint()
             
 
+a = Data_organizer()
+#a.make_tables()
+a.fill_tables()
