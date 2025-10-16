5703ea83226a60fdef74842b7253c618c0d41a3c
marcus.koseck <marcuskoseck@gmail.com>
2025-10-15 21:48:58 -0500

    made changes to parse data from api correctly and formatted a sql database.

--- src/scripts/collect_data.py
+++ src/scripts/collect_data.py
@@ -2,15 +2,16 @@ import urllib.request
 import json
 import numpy as np
 import pandas as pd
+from itertools import product
 
-def obtain_raw_data(pageNum):
+def obtain_raw_data(data_type,pageNum):
     base_url = "https://api.nal.usda.gov/fdc/v1/foods/list"
     api_key = "NhfPgJChrCiU2JaNEcZRTNkdkMw6RDKLhBsRarou"
     #query = "tomato"
     params = {
         "api_key": api_key,
         #"query": query,
-        "dataType": "SR Legacy", #Branded, Foundation, Survey (FNDDS), SR Legacy
+        "dataType": data_type, #Branded, Foundation, Survey (FNDDS), SR Legacy
         "pageSize": 200,
         "pageNumber": pageNum
     }
@@ -25,15 +26,20 @@ def obtain_raw_data(pageNum):
         data = json.loads(raw_data)
         #data.get("foods", [])  # List of food objects
         df = pd.json_normalize(data)
-        df.to_parquet(rf'C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data\dataframe_SR_Legacy.parquet', compression='brotli')
+        if not df.empty:
+            df.to_parquet(rf'C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data\dataframe_{data_type}_{pageNum}.parquet', compression='brotli')
 
     return data
 
-def save_raw_json(page_num,json_data):
-    df = pd.json_normalize(json_data)
-    df.to_parquet(rf'C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data\dataframe_{page_num}.parquet', compression='brotli')
+# def save_raw_json(page_num,json_data):
+#     df = pd.json_normalize(json_data)
+#     df.to_parquet(rf'C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data\dataframe_{page_num}.parquet', compression='brotli')
 
-for pageNum in range(1,2):
-    save_raw_json(pageNum,obtain_raw_data(pageNum))
+page_numbers = 1000
+different_food_types = data_types=["Branded"]#, "Foundation", "Survey (FNDDS)", "SR Legacy"]
+page_number_iters = range(300,page_numbers)
+
+for data_type,pageNum in product(different_food_types,page_number_iters):
+    obtain_raw_data(data_type,pageNum)
 
 
--- src/scripts/store_data.py
+++ src/scripts/store_data.py
@@ -21,26 +21,36 @@ class Data_organizer:
         return sqlite3.connect("USDA_foods.db")
 
     def branded_foods(self,df):
+        
         foods_df = df[["fdcId","description","dataType","publicationDate","gtinUpc"]]
         nutrients_df =  df[["gtinUpc","number","name","unitName","amount"]]
         brands_df = df[["gtinUpc","brandOwner"]]
+        
+        foods_df.to_sql(name='foods', con=self.con, if_exists='append', index=False)
+        nutrients_df.to_sql(name='nutrients', con=self.con, if_exists='append', index=False)
+        brands_df.to_sql(name='brands', con=self.con, if_exists='append', index=False)
+
 
     def foundational_foods(self,df):
-        foods_df = df[["fdcId","description","dataType","publicationDate","ndbNumber"]]
-        nutrients_df =  df[["ndbNumber","number","name","unitName","amount"]]
-        
+        df.rename(columns={"ndbNumber":"gtinUpc"},inplace=True)
+        foods_df = df[["fdcId","description","dataType","publicationDate","gtinUpc"]]
+        nutrients_df =  df[["gtinUpc","number","name","unitName","amount"]]
+
+        foods_df.to_sql(name='foods', con=self.con, if_exists='append', index=False)
+        nutrients_df.to_sql(name='nutrients', con=self.con, if_exists='append', index=False)
 
     def survey_foods(self,df):
-        foods_df = df[["fdcId","description","dataType","publicationDate","foodCode"]]
-        nutrients_df =  df[["foodCode","number","name","unitName","amount"]]
-    
+        df.rename(columns={"foodCode":"gtinUpc"},inplace=True)
+        foods_df = df[["fdcId","description","dataType","publicationDate","gtinUpc"]]
+        nutrients_df =  df[["gtinUpc","number","name","unitName","amount"]]
+        foods_df.to_sql(name='foods', con=self.con, if_exists='append', index=False)
+        nutrients_df.to_sql(name='nutrients', con=self.con, if_exists='append', index=False)
+
     def fill_tables(self):
         
         raw_data_dir = r"C:\Users\marcu\Documents\machine_learning_projects\recipe_recommender\data\raw_data"
         data_files = os.listdir(raw_data_dir)
-        all_foods_dfs = []
-        all_nut_dfs = []
-        all_food_nut_dfs = []
+
         for data_path in data_files:
             df = self.open_parquet(os.path.join(raw_data_dir,data_path))
             place_holder = df.explode("foodNutrients",ignore_index=True)
@@ -55,9 +65,7 @@ class Data_organizer:
             # TODO: save to sql tables.
             if data_type == "Branded":
                 self.branded_foods(df)
-            elif data_type == "Foundation":
-                self.foundational_foods(df)
-            elif data_type == "SR Legacy":
+            elif data_type == "Foundation" or data_type == "SR Legacy":
                 self.foundational_foods(df)
             elif data_type == "Survey (FNDDS)":
                 self.survey_foods(df)
@@ -79,3 +87,4 @@ class Data_organizer:
 a = Data_organizer()
 #a.make_tables()
 a.fill_tables()
+a.close_database()
